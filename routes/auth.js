789e57f0b6123906c990049eceb16de3f721f577
kkirtikaa <sharmakritika1105@gmail.com>
2025-02-15 22:02:33 +0530

    router,controller and model added

--- routes/auth.js
+++ routes/auth.js
@@ -1,1 +1,11 @@
-//const router = require('express').Routers();
+const { signup, login, forget,reset } = require('../controller/authcontroller');
+
+const router = require('express').Router();
+
+router.post('/login',login);
+router.post('/signup',signup);
+router.post('/forget',forget);
+router.put('/reset',reset);
+
+module.exports = router;
+
