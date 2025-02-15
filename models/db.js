6a88f67cf7a2afe3a68a5ff89e74faeebfb4bd43
kkirtikaa <sharmakritika1105@gmail.com>
2025-02-15 17:30:34 +0530

    database connected

---
+++ models/db.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+const mongo_url = process.env.MONGO_URL;
+mongoose.connect(mongo_url)
+.then(() => {
+    console.log('Database Connected');
+
+}).catch((err) => {
+     console.log('Error connecting to DB',err);
+})
